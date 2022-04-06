d9d4b12ebde070d2c13a75cdbac6f4b99523dafe
Antonio Antonino <antonio@kilt.io>
2022-04-06 09:40:15 +0200

    feat: add call proxy to DID pallet

--- pallets/did/src/did_details.rs
+++ pallets/did/src/did_details.rs
@@ -20,12 +20,12 @@ use codec::{Decode, Encode, MaxEncodedLen, WrapperTypeEncode};
 use frame_support::{
 	ensure,
 	storage::{bounded_btree_map::BoundedBTreeMap, bounded_btree_set::BoundedBTreeSet},
-	traits::Get,
+	traits::Get, dispatch::Weight,
 };
 use kilt_support::deposit::Deposit;
 use scale_info::TypeInfo;
 use sp_core::{ecdsa, ed25519, sr25519};
-use sp_runtime::{traits::Verify, MultiSignature};
+use sp_runtime::{traits::Verify, MultiSignature, DispatchError};
 use sp_std::convert::TryInto;
 
 use crate::{
@@ -549,7 +549,22 @@ pub enum RelationshipDeriveError {
 	InvalidCallParameter,
 }
 
-pub type DeriveDidCallKeyRelationshipResult = Result<DidVerificationKeyRelationship, RelationshipDeriveError>;
+pub enum DidVerificationType {
+	Inline,
+	StoredVerificationKey(DidVerificationKeyRelationship),
+}
+
+impl DidVerificationType {
+	pub fn inline() -> Self {
+		Self::Inline
+	}
+
+	pub fn with_verification_key(relationship: DidVerificationKeyRelationship) -> Self {
+		Self::StoredVerificationKey(relationship)
+	}
+}
+
+pub type DeriveDidVerificationTypeResult = Result<DidVerificationType, RelationshipDeriveError>;
 
 /// Trait for extrinsic DID-based authorization.
 ///
@@ -558,10 +573,10 @@ pub type DeriveDidCallKeyRelationshipResult = Result<DidVerificationKeyRelations
 /// extrinsic to specify what DID key to use to perform signature validation
 /// over the byte-encoded operation. A result of None indicates that the
 /// extrinsic does not support DID-based authorization.
-pub trait DeriveDidCallAuthorizationVerificationKeyRelationship {
+pub trait DeriveDidCallAuthorizationVerificationType {
 	/// The type of the verification key to be used to validate the
 	/// wrapped extrinsic.
-	fn derive_verification_key_relationship(&self) -> DeriveDidCallKeyRelationshipResult;
+	fn derive_verification_key_relationship(&self) -> DeriveDidVerificationTypeResult;
 
 	// Return a call to dispatch in order to test the pallet proxy feature.
 	#[cfg(feature = "runtime-benchmarks")]
@@ -625,3 +640,9 @@ impl<T: Config> core::ops::Deref for DidAuthorizedCallOperationWithVerificationR
 // [DidAuthorizedCallOperationWithVerificationRelationship] encodes to
 // [DidAuthorizedCallOperation].
 impl<T: Config> WrapperTypeEncode for DidAuthorizedCallOperationWithVerificationRelationship<T> {}
+
+pub trait DidCallProxy<T: Config> {
+	fn weight(call: &DidCallableOf<T>) -> Weight;
+
+	fn authorise(call: &DidCallableOf<T>, did: &DidIdentifierOf<T>, signature: &DidSignature) -> Result<(), DispatchError>;
+}
